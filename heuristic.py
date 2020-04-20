8ef84ce2dd009839446eb63a42fa2c052e95b9c0
Abdelhakim Cheriet <ahcheriet@gmail.com>
2020-04-20 22:01:09 +0100

    Debugin

--- heuristic.py
+++ heuristic.py
@@ -39,6 +39,14 @@ def neighbor(point, target_tics_):
     return adjacents
 
 
+def get_neighbor_list(initial_list,target_tics):
+    anchors_list = []
+    neighbor_list = [neighbor(x, target_tics_=target_tics) for x in initial_list]
+    for items in itertools.product(*neighbor_list):
+        anchors_list.append(list(items))
+    return anchors_list
+
+
 # Param
 # For every number of anchors, we need to initialise the initial vector which gives the optimal
 # anchor placement for the low discretisation.
@@ -46,61 +54,44 @@ def neighbor(point, target_tics_):
 # initial =[(0, 6510), (6510, 0), (6510, 6510)]  # tics=3255 anchors=3 4*4
 # Remember that [(3255, 0), (3255, 9765), (9765, 6510)] = [(7,0),(7,21),(21,14)]
 # initial = [(6510, 3255), (6510, 9765), (9765, 0), (9765, 9765)] #4 anchors 4*4
-
+initial = [(0, 12), (12, 12), (12, 24)] #3 anchors 3*3
 
 # print(len(positions))
 # drawNetwork([positions[200]], algo_="initial", mode_="show")
 # drawNetwork(neighbor(positions[200]), algo_="n1", mode_="show")
-
-# for choice in [7]:#,15,31]:
-# choice: int = 7
-# tics = tics // choice
-# anchors_list = []
-# neighbor_list = [neighbor(x, step_=2) for x in initial]
-# for items in itertools.product(*neighbor_list):
-#     anchors_list.append(list(items))
-
-# anchors_list = []
-# neighbor_list = [neighbor(x, initial_tics_=12, target_tics_=6) for x in initial]
-# for items in itertools.product(*neighbor_list):
-#     anchors_list.append(list(items))
-
-
-n0 = neighbor((12, 12), target_tics_=3)
-print(n0)
-drawNetwork(n0, mode_='dirte')
 # into_list = list(itertools.chain(*anchors_list))
-
 # #drawNetwork(initial, algo_="initial", mode_="show")
 # #into_list = list(itertools.chain(*anchors_list))
 # #drawNetwork(into_list, algo_="nl", mode_="show")
-#
-#
-# minAvgRA = 999999999
-# optimal_anchors = []
-# start = time.time()
-# for index, anchors in enumerate(tqdm(anchors_list)):
-#     l = getAllSubRegions(anchors_=anchors, max_x_=max_x, max_y_=max_y)
-#     res = getDisjointSubRegions(l)
-#     avgRA = getExpectation(res)
-#     if avgRA != 0:
-#         if minAvgRA > avgRA:
-#             minAvgRA = avgRA
-#             optimal_anchors = []
-#             for a in anchors:
-#                 optimal_anchors.append(a)
-#             optimal_areas = res
-# end = time.time()
-#
-# drawNetwork(optimal_anchors, optimal_areas, algo_="heuristic")
-#
-# print("**Optimal Anchor Pos.:" + str(optimal_anchors), minAvgRA)
-# print('Runinig Times : ' + str(round((end - start) / 60.0, 2)) + ' (min.)')
-#
-# f_res = open('./TXT/heuristic.txt', 'a')
-# f_res.write(str(optimal_anchors) + ';' + str(minAvgRA) + ';' + str(end - start) + ';' + str(NB_ANCHORS) + ';' + str(
-#     tics) + '\n')
-# f_res.close()
-#
-# ##TODO The initial point is from brute force, I find it by multiplying it with the TICS.
-# ##TODO I should verify whether the results of the neigbhoor function is reduced to the current TICS
+
+anchors_list = get_neighbor_list(initial, target_tics=6)
+
+
+minAvgRA = 999999999
+optimal_anchors = []
+start = time.time()
+for index, anchors in enumerate(tqdm(anchors_list)):
+    l = getAllSubRegions(anchors_=anchors, max_x_=max_x, max_y_=max_y)
+    res = getDisjointSubRegions(l)
+    avgRA = getExpectation(res)
+    if avgRA != 0:
+        if minAvgRA > avgRA:
+            minAvgRA = avgRA
+            optimal_anchors = []
+            for a in anchors:
+                optimal_anchors.append(a)
+            optimal_areas = res
+end = time.time()
+
+drawNetwork(optimal_anchors, optimal_areas, algo_="heuristic")
+
+print("**Optimal Anchor Pos.:" + str(optimal_anchors), minAvgRA)
+print('Runinig Times : ' + str(round((end - start) / 60.0, 2)) + ' (min.)')
+
+f_res = open('./TXT/heuristic.txt', 'a')
+f_res.write(str(optimal_anchors) + ';' + str(minAvgRA) + ';' + str(end - start) + ';' + str(NB_ANCHORS) + ';' + str(
+    tics) + '\n')
+f_res.close()
+
+##TODO The initial point is from brute force, I find it by multiplying it with the TICS.
+##TODO I should verify whether the results of the neigbhoor function is reduced to the current TICS
