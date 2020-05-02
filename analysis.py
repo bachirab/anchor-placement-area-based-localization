4c30880dcaee0b4802a46475d4c1fe11287cf763
Abdelhakim Cheriet <ahcheriet@gmail.com>
2020-05-02 17:07:08 +0100

    Updating the f_heuristic from local

--- analysis.py
+++ analysis.py
@@ -2,28 +2,35 @@ from our_library import *
 import pandas as pd
 import matplotlib.pylab as plt
 
-b_data = "TXT/brute.txt"
+b_data = "TXT/brutee.txt"
 fh_data = "TXT/fear_heuristic.txt"
 h_data = "TXT/heuristic.txt"
 g_data = "TXT/genetic.txt"
 pg_data = "TXT/p_genetic.txt"
+#pg_data = "TXT/P_GENETIC.TXT"
 
-worst = [[0,0],[0,192],[192,192]]
-l = getAllSubRegions(anchors_=[(0, 96), (96, 0), (96, 96), (192, 96)])
-res = getDisjointSubRegions(l)
-drawNetwork(worst, res, mode_="ss")
-avgRA = getExpectation(res)
-entr, px_ = get_entropy(res)
-print(avgRA, entr)
-input()
 
 def draw_comparison(df, nb_anchors, tics, algorithms: list):
-    df.groupby(['nb_anchors', 'tics', 'algorithm']).mean().loc[nb_anchors,:,:]['Expectation']\
-        .unstack().plot.bar()
-    plt.show()
-    df.groupby(['nb_anchors', 'tics', 'algorithm']).mean().loc[nb_anchors,:12, :]['Time']\
-        .unstack().plot.bar()
-    plt.show()
+    fig, ax = plt.subplots()
+    df.groupby(['nb_anchors', 'tics', 'algorithm']).min().loc[nb_anchors,:,:]['Expectation']\
+        .unstack().plot.bar(ax=ax)
+   # ax.legend(["Brute","Genetic","Heuristic","GGGG"])
+    labels = ["2","6","12","24","48","96"]
+    ax.set_xticklabels(labels)
+    ax.set_xlabel("Descritization")
+    ax.set_ylabel("Expected area")
+#    plt.show()
+    plt.savefig('IMG/accuracy_'+str(nb_anchors)+".pdf")
+    fig, ax = plt.subplots()
+    df.groupby(['nb_anchors', 'tics', 'algorithm']).min().loc[nb_anchors,:, :]['Time']\
+        .unstack().plot.bar(log=True,ax=ax)
+  #  ax.legend(["Brute","Genetic","Heuristic"])
+    labels = ["2","6","12","24","48","96"]
+    ax.set_xticklabels(labels)
+    ax.set_xlabel("Descritization")
+    ax.set_ylabel("time in (s)")
+#    plt.show()
+    plt.savefig('IMG/time_'+str(nb_anchors)+".pdf")
 
 
 def get_min_anchor(df, nb_anchors, tics, algorithm="pgenetic"):
@@ -45,12 +52,12 @@ def draw_and_accuracy(df, nb_anchors, tics, algorithm):
     res = getDisjointSubRegions(l)
     avgRA = getExpectation(res)
     entr, px_ = get_entropy(res)
-    drawNetwork(min_anchors, res, nb_anchors_=nb_anchors, tics_=tics, algo_=algorithm, mode_="ss")
-    s = pd.Series(px_)
-    s.plot.kde()
-    plt.show()
-    s.plot.hist(cumulative=True, density=True)
-    plt.show()
+    drawNetwork(min_anchors, res, nb_anchors_=nb_anchors, tics_=tics, algo_=algorithm)
+#    s = pd.Series(px_)
+#    s.plot.kde()
+#    plt.show()
+#    s.plot.hist(cumulative=True)
+#    plt.show()
     return avgRA, entr
 
 
@@ -65,18 +72,22 @@ pg_df = pd.read_csv(pg_data, sep=";", names=["Anchors", "Expectation", "Time", "
 h_df = pd.read_csv(h_data, sep=";", names=["Anchors", "Expectation", "Time", "nb_anchors", "tics"])\
     .sort_values('tics', ascending=False)
 b_df["algorithm"] = ["Brute force" for _ in range(len(b_df))]
-fh_df["algorithm"] = ["fair heuristic" for _ in range(len(fh_df))]
+fh_df["algorithm"] = ["Heuristic" for _ in range(len(fh_df))]
 g_df["algorithm"] = ["Genetic" for _ in range(len(g_df))]
-pg_df["algorithm"] = ["pgenetic" for _ in range(len(pg_df))]
+pg_df["algorithm"] = ["Genetic" for _ in range(len(pg_df))]
 h_df["algorithm"] = ["Heuristic" for _ in range(len(h_df))]
-All_df = pd.concat([b_df, fh_df, h_df, pg_df,g_df])
+#All_df = pd.concat([b_df, fh_df, h_df, pg_df,g_df])
+All_df = pd.concat([b_df, fh_df, pg_df])
+
 
 for nb_anchors_ in [3, 4, 5]:
     draw_comparison(All_df, nb_anchors=nb_anchors_, tics=96, algorithms=[])
 
-draw_and_accuracy(All_df, nb_anchors=3, tics=96, algorithm="pgenetic")
-
-draw_and_accuracy(All_df, nb_anchors=3, tics=96, algorithm="Brute force")
+# for algo in ["Heuristic","Brute force"]:
+#     for nb_anchors_ in [5, 4, 5]:
+#         for tics_ in [48,24,12,6,2]:
+#             print(nb_anchors_," ",tics_)
+#             draw_and_accuracy(All_df, nb_anchors=nb_anchors_, tics=tics_, algorithm=algo)
 
 # All_df.groupby(['nb_anchors', 'tics', 'algorithm']).mean().loc[4, :12, :]['minavrg'].unstack().plot.bar()
 # plt.show()
